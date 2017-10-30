7fbb12fd436fcc4723ef1cefb762dbcc967deca0
Iliya Zhechev <i4ko95@gmail.com>
2017-10-30 21:52:40 +0200

    refactoring

--- sliding-blocks.py
+++ sliding-blocks.py
@@ -16,7 +16,7 @@ class Graph:
         current = end
         while current != start:
             yield current
-            current= predecessor[current]
+            current = predecessor[current]
 
     def a_star(self, start, end):
         predecessor = {}
@@ -44,10 +44,13 @@ class Graph:
 
 class SlidingBlocksGraph(Graph):
 
-    def solve(self, start):
+    def __init__(self, start):
+        self.start = tuple(tuple(el for el in row) for row in start)
         cols, rows = self.get_board_shape(start)
-        end = self.get_board_end(cols, rows)
-        solution_path = self.a_star(start, end)
+        self.end = self.get_board_end(cols, rows)
+
+    def solve(self):
+        solution_path = self.a_star(self.start, self.end)
         solution = self.convet_path_to_moves(solution_path)
         return list(solution)
 
@@ -56,79 +59,86 @@ class SlidingBlocksGraph(Graph):
 
     def get_board_end(self, cols, rows):
         size = cols * rows
-        return [[(r * c) % size for c in range(cols)] for r in range(rows)]
+        return tuple(tuple((r * c) % size for c in range(cols))
+                                          for r in range(rows))
 
     def convet_path_to_moves(self, solution_path):
         for src, dst in zip(solution_path, solution_path[1:]):
             yield self.get_move_between_boards(src, dst)
 
-    def get_move_between_boards(self, srx, dst):
+    def get_children(self, vertex):
+        pass
+
+    def heuristic(self, current, goal):
+        pass
+
+    def get_move_between_boards(self, src, dst):
         pass
 
 
 class SlidingBlocksTests(unittest.TestCase):
 
     def test_single_square_solved_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([[0]]), [])
+        self.assertEqual(SlidingBlocksGraph([[0]]).solve(), [])
 
     def test_single_square_unsolvable_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([[1]]), [])
+        self.assertEqual(SlidingBlocksGraph([[1]]).solve(), [])
 
     def test_four_square_solved_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [1, 2],
             [3, 0]
-        ]), [])
+        ]).solve(), [])
 
     def test_four_square_unsolvable_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [3, 2], 
             [1, 0]
-        ]), [])
+        ]).solve(), [])
 
     def test_four_square_complex_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [2, 3], 
             [1, 0]
-        ]), ['down', 'right', 'up', 'left'])
+        ]).solve(), ['down', 'right', 'up', 'left'])
 
     def test_single_row_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [0, 1, 2, 3]
-        ]), ['left', 'left', 'left'])
+        ]).solve(), ['left', 'left', 'left'])
 
     def test_single_col_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
-            [[0],
-             [1],
-             [2],
-             [3]]
-        ]), ['up', 'up', 'up'])
+        self.assertEqual(SlidingBlocksGraph([
+            [0],
+            [1],
+            [2],
+            [3]
+        ]).solve(), ['up', 'up', 'up'])
 
     def test_rectangle_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [1, 2, 3],
             [0, 4, 5]
-        ]), ['left', 'left'])
+        ]).solve(), ['left', 'left'])
 
     def test_example_board(self):
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [1, 2, 3],
             [4, 5, 6],
             [0, 7, 8]
-        ]), ['left', 'left'])
+        ]).solve(), ['left', 'left'])
 
     def test_complex_board(self):
         # TODO: Think of complex example
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [1, 2, 3]
-        ]), [])
+        ]).solve(), [])
 
     def test_big_complex_board(self):
         # TODO: Think of complex example
-        self.assertEqual(SlidingBlocksGraph().solve([
+        self.assertEqual(SlidingBlocksGraph([
             [1, 2, 3]
-        ]), [])
+        ]).solve(), [])
 
 
 if __name__ == '__main__':
