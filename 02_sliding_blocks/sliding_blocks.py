83f9e46c31d193924c190718ca4fcbd1fac55d48
Iliya Zhechev <i4ko95@gmail.com>
2017-11-02 21:43:44 +0200

    heuristic

--- 02_sliding_blocks/sliding_blocks.py
+++ 02_sliding_blocks/sliding_blocks.py
@@ -53,7 +53,7 @@ class SlidingBlocksGraph(Graph):
         self.cols = cols
         self.rows = rows
         self.end = self.get_board_end(cols, rows)
-        self.zero_pos_x, self.zero_pos_y = self.get_zero_pos(self.end)
+        self.zero_pos_x, self.zero_pos_y = self.get_elem_pos(self.end)
 
     def solve(self):
         solution_path = list(self.a_star(self.start, self.end))
@@ -74,7 +74,7 @@ class SlidingBlocksGraph(Graph):
 
     def get_children(self, state):
         result = []
-        x, y = self.get_zero_pos(state)
+        x, y = self.get_elem_pos(state)
 
         if y > 0: result.append(self.swap(state, x, y, x, y - 1))
         if y < self.rows - 1: result.append(self.swap(state, x, y, x, y + 1))
@@ -92,41 +92,39 @@ class SlidingBlocksGraph(Graph):
         return tuple(tuple(el for el in row) for row in new_state), 1
 
     def heuristic(self, current, goal):
+        total = 0
+
+        for elem in range(self.rows * self.cols - 1):
+            x_cur, y_cur = self.get_elem_pos(current, elem + 1)
+            x_goal, y_goal = self.get_elem_pos(goal, elem + 1)
+            total = total + abs(x_cur - x_goal) + abs(y_cur - y_goal)
+
         return 0
 
     def get_move_between_boards(self, src, dst):
-        src_zero_x, src_zero_y = self.get_zero_pos(src)
-        dst_zero_x, dst_zero_y = self.get_zero_pos(dst)
+        src_zero_x, src_zero_y = self.get_elem_pos(src)
+        dst_zero_x, dst_zero_y = self.get_elem_pos(dst)
         dx, dy = src_zero_x - dst_zero_x, src_zero_y - dst_zero_y
         if dx == -1: return 'left'
         if dx == 1: return 'right'
         if dy == -1: return 'up'
         if dy == 1: return 'down'
 
-    def get_zero_pos(self, state):
+    def get_elem_pos(self, state, elem=0):
         for y_pos in range(len(state)):
-            if 0 in state[y_pos]:
-                return state[y_pos].index(0), y_pos
+            if elem in state[y_pos]:
+                return state[y_pos].index(elem), y_pos
         return -1, -1
 
 
 def input_board(size):
-    board = [[]]
-    for _ in range(size):
-        row_id = len(board) - 1
-        for _ in range(size):
-            cell = int(input())
-            board[row_id].append(cell)
-        board.append([])
-    board.pop()
-
-    return board
+    return [list(map(lambda n: int(n), input().split(' ')))
+            for _ in range(size)]
 
 
 if __name__ == '__main__':
     size = int(input())
     board = input_board(size)
-    print(board)
     solution = SlidingBlocksGraph(board).solve()
 
     print('\n'.join(solution))
